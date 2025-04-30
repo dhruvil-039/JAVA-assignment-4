a6f6729c8c19b1306ab50521132b83d0e648a994
Dhruvil Galaiya <dhruvil.galaiya.btech2023@sitpune.edu.in>
2025-04-30 15:09:22 +0530

    Add files via terminal
    Added Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+/*
+Name: Dhruvil Galaiya
+PRN: 23070126039
+Batch: A2
+
+This is the main class to run the Card Deck program using menu-driven operations.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        Deck deck = new Deck(); // createDeck() is called inside constructor
+        deck.menu();
+    }
+}
